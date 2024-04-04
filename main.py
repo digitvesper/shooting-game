3b24b584b56aa593452e163a8cccd122e69d727d
digitvesper <digitvesper@gmail.com>
2024-04-04 11:30:48 +0300

    add new const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,7 +7,17 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Игра тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/tyr.png")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 50
+target_height = 50
+
+target_x = random.randint(0, screen_width - target_width)
+target_y = random.randint(0, screen_height - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 
